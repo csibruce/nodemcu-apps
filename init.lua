2b0fc3ec7bd62eb7379722cd53f17fdcc76550ab
bruce <bruce@socar.kr>
2017-02-11 00:09:07 +0900

    add lcd1602.lua

--- init.lua
+++ init.lua
@@ -2,9 +2,19 @@ dofile("credentials.lua")
 dofile("slack.lua")
 bluePin = 1
 redPin = 2
+serverCount = 0
 gpio.mode(bluePin,gpio.OUTPUT)
 gpio.mode(redPin,gpio.OUTPUT)
+ 
+i2c.setup(0, 3, 4, i2c.SLOW)
+lcd = dofile("lcd1602.lua")()
 
+lcd:put(lcd:locate(0, 0), "SSID:"); lcd:put(lcd:locate(0, 6), "none")
+lcd:put(lcd:locate(1, 0), "IP:");   lcd:put(lcd:locate(1, 4), "none")
+ 
+function printServerState(count)
+  lcd:put(lcd:locate(3, 0), "server on("..count..")")
+end
 
 function blinkRed()
   gpio.write(redPin,1)
@@ -23,10 +33,10 @@ function startup()
     print("init.lua deleted")
   else
     print("Running")
+    lcd:put(lcd:locate(3, 0), "server on")
     file.close("init.lua")
     blinkRed()
-    blinkBlue()
-    slack.send(SLACK_ID)
+    blinkBlue()   
     dofile("webserver.lua")
   end
 end
@@ -39,9 +49,13 @@ print("set up wifi mode")
 wifi.setmode(wifi.STATION)
 wifi.sta.config(SSID,PASSWORD,0)
 wifi.sta.connect()
+
+second = 0
 tmr.alarm(1, 1000, 1, function()
   if wifi.sta.getip()== nil then
     print("IP unavaiable, Waiting...")
+    lcd:put(lcd:locate(2, 0), "IP unavaiable("..second..")")
+    second=second+1
     print(SLACK_ID)
     blinkRed()
   else
@@ -49,6 +63,10 @@ tmr.alarm(1, 1000, 1, function()
     print("Config done, IP is "..wifi.sta.getip())
     print("You have 5 seconds to abort Startup")
     print("Waiting...")
+    slack.send(SLACK_ID)
+    lcd:put(lcd:locate(0, 5), " "..wifi.sta.getdefaultconfig())
+    lcd:put(lcd:locate(1, 3), " "..wifi.sta.getip())
+    lcd:put(lcd:locate(2, 0), "---Wifi Connected---")
     tmr.alarm(0,5000,0,startup)
   end
  end)
--- webserver.lua
+++ webserver.lua
@@ -3,6 +3,8 @@ srv:listen(80, function(conn)
   conn:on("receive", function(sck, payload)
     print(payload)
     blinkBlue()
+    printServerState(serverCount)
+    serverCount=serverCount+1
     sck:send("HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n<h1> Hello, NodeMCU & Bruce~!.</h1>")
   end)
 
